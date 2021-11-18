c77b089ba823413eb306ab6382cebb9440ae5ccd
YottyHol <git@oholliday.co.uk>
2021-11-18 00:23:51 +0000

    firebase hosting

--- exec.ps1
+++ exec.ps1
@@ -1,2 +1,2 @@
-docker build --rm -t auth0-vue-01-login .
-docker run -p 3000:3000 --pid=host auth0-vue-01-login
+docker build --rm -t auth0-vue-Yotty-App .
+docker run -p 3000:3000 --pid=host auth0-vue-Yotty-App
